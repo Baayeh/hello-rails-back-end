9ece2aa0a2a4d8b511089f8e10722014f05bf9a4
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-20 09:35:36 +0000

    Generate random greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create([
+  { message: "Hey there! Thanks for leaving us a message." },
+  { message: "Hello and welcome! We're excited to hear from you." },
+  { message: "Greetings! Your message has been received." },
+  { message: "Hi, thank you for contacting us. We'll get back to you as soon as possible." },
+  { message: "Good day! We appreciate you reaching out to us and will respond shortly." }
+])
